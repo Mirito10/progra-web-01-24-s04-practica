49892997d5e5f315db4c27247b5864ef3698db9e
Mirito10 <mathias.calderon@ulatina.net>
2024-02-08 20:30:28 -0600

    Mathias Calderon

---
+++ Mcalderon/mcalderon/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+let nombre ="Mathias";
+nombre = "Danilo";
+
+Function despedir (){
+  alert("Adios");
+}
+
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1> {nombre}
+      <input type="text"></input>
+      <input type ="password"></input>
+      <br></br>
+      <select>
+        <option>1</option>
+        <option>2</option>
+      </select>
+      <br></br>
+      <button onClick={saludar}Saludar></button>
+      <button onClick={despedir}Despedir></button>
+    </div>
+  );
+}
+
+export default App;
